15dd3657dc83a14dd7d4e0c9784c47a17fc3049e
tiago <tigolimab223@gmail.com>
2024-07-14 19:00:37 -0300

    chore: configurationg

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,7 @@
-import express, { Request, Response, NextFunction } from "express"
+import app from "./app.js"
 
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get("/", (req, res, next) => {
-  try {
-    return res.send({ message: "Hello" })
-  } catch (error) {
-    return next(error)
-  }
-})
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (process.env.NODE_ENV === "development") {
-    console.error(err)
-  }
-  res.status(500).send({
-    message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
-  })
-})
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  /* First argument is just color, second is the message */
   console.log("\x1b[32m", `Server is listening on port ${PORT}`)
 })
